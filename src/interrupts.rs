cacb890ed4023ddcff490be875125a9f8b93132a
seanrickard <sean.rickard26@gmail.com>
2020-04-11 23:00:07 -0400

    paging intro

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,9 +1,11 @@
 use crate::gdt;
+use crate::hlt_loop;
 use crate::print;
 use crate::println;
 use lazy_static::lazy_static;
 use pic8259_simple::ChainedPics;
 use spin;
+use x86_64::structures::idt::PageFaultErrorCode;
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 
 pub const PIC_1_OFFSET: u8 = 32;
@@ -11,6 +13,10 @@ pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8;
 
 pub static PICS: spin::Mutex<ChainedPics> =
     spin::Mutex::new(unsafe { ChainedPics::new(PIC_1_OFFSET, PIC_2_OFFSET) });
+
+pub fn init_idt() {
+    IDT.load();
+}
 
 lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
@@ -26,34 +32,55 @@ lazy_static! {
         idt[InterruptIndex::Keyboard.as_usize()]
         .set_handler_fn(keyboard_interrupt_handler);
 
+        idt.page_fault.set_handler_fn(page_fault_handler);
+
         idt
     };
 }
 
-pub fn init_idt() {
-    IDT.load();
+#[cfg(test)]
+use crate::{serial_print, serial_println};
+
+#[test_case]
+fn test_breakpoint_exception() {
+    serial_print!("test_breakpoint_exception...");
+    // invoke a breakpoint exception
+    x86_64::instructions::interrupts::int3();
+    serial_println!("[ok]");
 }
 
-extern "x86-interrupt" fn breakpoint_handler(stack_frame: &mut InterruptStackFrame) {
-    println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
+#[derive(Debug, Clone, Copy)]
+#[repr(u8)]
+pub enum InterruptIndex {
+    Timer = PIC_1_OFFSET,
+    Keyboard,
 }
 
-extern "x86-interrupt" fn double_fault_handler(
-    stack_frame: &mut InterruptStackFrame,
-    _error_code: u64,
-) -> ! {
-    panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
-}
+impl InterruptIndex {
+    fn as_u8(self) -> u8 {
+        self as u8
+    }
 
-extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: &mut InterruptStackFrame) {
-    print!(".");
-
-    unsafe {
-        PICS.lock()
-            .notify_end_of_interrupt(InterruptIndex::Timer.as_u8());
+    fn as_usize(self) -> usize {
+        usize::from(self.as_u8())
     }
 }
 
+//page faults
+extern "x86-interrupt" fn page_fault_handler(
+    stack_frame: &mut InterruptStackFrame,
+    error_code: PageFaultErrorCode,
+) {
+    use x86_64::registers::control::Cr2;
+
+    println!("EXCEPTION: PAGE FAULT");
+    println!("Accessed Address: {:?}", Cr2::read());
+    println!("Error Code: {:?}", error_code);
+    println!("{:#?}", stack_frame);
+    hlt_loop();
+}
+
+// keyboard interrupts
 extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: &mut InterruptStackFrame) {
     use pc_keyboard::{layouts, DecodedKey, HandleControl, Keyboard, ScancodeSet1};
     use spin::Mutex;
@@ -84,30 +111,25 @@ extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: &mut Interrup
     }
 }
 
-#[cfg(test)]
-use crate::{serial_print, serial_println};
-
-#[test_case]
-fn test_breakpoint_exception() {
-    serial_print!("test_breakpoint_exception...");
-    // invoke a breakpoint exception
-    x86_64::instructions::interrupts::int3();
-    serial_println!("[ok]");
+// breakpoint handler
+extern "x86-interrupt" fn breakpoint_handler(stack_frame: &mut InterruptStackFrame) {
+    println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
 }
 
-#[derive(Debug, Clone, Copy)]
-#[repr(u8)]
-pub enum InterruptIndex {
-    Timer = PIC_1_OFFSET,
-    Keyboard,
+//double fault handler
+extern "x86-interrupt" fn double_fault_handler(
+    stack_frame: &mut InterruptStackFrame,
+    _error_code: u64,
+) -> ! {
+    panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
 }
 
-impl InterruptIndex {
-    fn as_u8(self) -> u8 {
-        self as u8
-    }
+// timer interrupt handler
+extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: &mut InterruptStackFrame) {
+    print!(".");
 
-    fn as_usize(self) -> usize {
-        usize::from(self.as_u8())
+    unsafe {
+        PICS.lock()
+            .notify_end_of_interrupt(InterruptIndex::Timer.as_u8());
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -17,8 +17,29 @@ pub extern "C" fn _start() -> ! {
         stack_overflow(); // for each recursion, the return address is pushed
     }
 
+    use x86_64::registers::control::Cr3;
+
+    let (level_4_page_table, _) = Cr3::read();
+    println!("Level 4 page table at: {:?}", level_4_page_table.start_address());
     // uncomment line below to trigger a stack overflow
     // stack_overflow();
+
+    // trigger a page fault
+
+    // let ptr = 0x203142 as *mut u32;
+
+    // read from a code page
+    // unsafe {
+    //     let x = *ptr;
+    // }
+    // println!("read worked");
+
+    // // write to a code page
+    // unsafe { *ptr = 42; }
+    // println!("write worked");
+
+    //let ptr = 0xdeadbeaf as *mut u32;
+    //unsafe { *ptr = 42; }
 
     #[cfg(test)]
     test_main();
